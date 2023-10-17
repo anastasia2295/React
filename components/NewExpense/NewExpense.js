2d7c18173840a988787ab3d474b5d791ba0cca59
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-10-17 19:15:49 +0300

    Добавление Cancel for new elementa

--- components/NewExpense/NewExpense.js
+++ components/NewExpense/NewExpense.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) =>{
+    const [isEditing, setIsEditing] = useState(false)
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         }
         props.onAppExpense(expenseData)
+        setIsEditing(false)
+    };
+    const startEditingHandler = () => {
+        setIsEditing(true)
+    }
+    const stopEditingHandler = () => {
+        setIsEditing(false)
     }
 return <div className="new-expense">
-<ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+   {!isEditing && <button onClick={startEditingHandler}>Add New Epxense</button>}
+{isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}/>}
 </div>
 };
 
