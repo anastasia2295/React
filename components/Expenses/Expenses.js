aefce815278ad8a16efe210d11a90e1b64125f9e
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-10-17 18:50:35 +0300

    Добавление операторов условного возврата

--- components/Expenses/Expenses.js
+++ components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
-import ExpenseItem from './expenseItem';
 import Card from '../Ul/Card';
-import './Expenses.css';
 import ExpensesFilter from './ExpenseFilter';
+import ExpensesList from './ExpensesList';
+import './Expenses.css';
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020")
@@ -11,19 +11,19 @@ function Expenses(props) {
    const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear)
    }
+
+   const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear
+   });
+
   return (
     <div>
     <Card className="expenses">
       <ExpensesFilter 
       selected={filteredYear} 
-      onChangeFilter={filterChangeHandler}/>
-      {props.items.map((expense) => (
-      <ExpenseItem 
-      title={expense.title} 
-      amount={expense.amount} 
-      date={expense.date}
+      onChangeFilter={filterChangeHandler}
       />
-      ))}
+      <ExpensesList items= {filteredExpenses}/>
     </Card>
     </div>
   );
