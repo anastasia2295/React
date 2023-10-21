2f04d2aa16e6eae400756972852e994159765238
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-10-21 09:27:37 +0300

    Другой способ подъема state и получения данных с п

--- src/App.js
+++ src/App.js
@@ -4,38 +4,38 @@ import ResultTable from "./components/ResultTable/ResultTable";
 import UserInput from "./components/UserInput/UserInput";
 
 function App() {
+  const [userInput, setUserInput] = useState(null)
   const calculateHandler = (userInput) => {
-    const [results, setResults] = useState(null)
-
-    const yearlyData = []; 
-
-    let currentSavings = +userInput['current-savings']; 
-    const yearlyContribution = +userInput['yearly-contribution']; 
-    const expectedReturn = +userInput['expected-return'] / 100;
-    const duration = +userInput['duration'];
-
-    for (let i = 0; i < duration; i++) {
-      const yearlyInterest = currentSavings * expectedReturn;
-      currentSavings += yearlyInterest + yearlyContribution;
-      yearlyData.push({
-     
-        year: i + 1,
-        yearlyInterest: yearlyInterest,
-        savingsEndOfYear: currentSavings,
-        yearlyContribution: yearlyContribution,
-      });
-    }
-setResults(yearlyData)
-
+    setUserInput(userInput)
   };
+
+  const yearlyData = []; 
+if (userInput){
+  let currentSavings = +userInput['current-savings']; 
+  const yearlyContribution = +userInput['yearly-contribution']; 
+  const expectedReturn = +userInput['expected-return'] / 100;
+  const duration = +userInput['duration'];
+
+  for (let i = 0; i < duration; i++) {
+    const yearlyInterest = currentSavings * expectedReturn;
+    currentSavings += yearlyInterest + yearlyContribution;
+    yearlyData.push({
+   
+      year: i + 1,
+      yearlyInterest: yearlyInterest,
+      savingsEndOfYear: currentSavings,
+      yearlyContribution: yearlyContribution,
+    });
+  }
+}
 
   return (
     <div>
       <Header/>
 
       <UserInput onCalculate={calculateHandler} />
-
-      <ResultTable/>
+      {!userInput && <p>No investment calculated yet</p>}
+      {userInput && <ResultTable/>}
     </div>
   );
 }
