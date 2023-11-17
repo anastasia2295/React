7e3bda94237d2c607cd5648d08a322fca2904f24
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-11-17 08:19:10 +0300

    Add files via upload

--- React+TS/Раздел3_project/SecondProject/src/App.jsx
+++ React+TS/Раздел3_project/SecondProject/src/App.jsx
@@ -1,6 +1,35 @@
+import Header from "./components/Header"
+import UserInput from "./components/UserInput"
+import { useState } from "react"
+import Result from "./components/Result"
+
 function App() {
+
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 1000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+})
+
+const inputIsValid = userInput >=1
+
+function handlerChange(inputIdentifier, newValue){
+setUserInput(prevUserInput => {
+    return{
+        ...prevUserInput,
+        [inputIdentifier]: +newValue
+    }
+})
+}
+
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+    <Header></Header>
+    <UserInput userInput={userInput} onChange={handlerChange}></UserInput>
+    {!inputIsValid && <p className="center">Please enter valid input data</p>}
+    {inputIsValid && <Result input={userInput}></Result>}
+    </>
   )
 }
 
