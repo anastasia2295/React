7457db767ad87ac62bab9bdd42b989942124a7a2
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-11-26 20:15:14 +0300

    Проверка пользов ввода и отображ модал окна ошибк
    Проверка пользовательского ввода и отображение модального окна ошибки

--- Раздел9_Practice/src/components/NewProject.jsx
+++ Раздел9_Practice/src/components/NewProject.jsx
@@ -1,8 +1,11 @@
 import Input from './Input.jsx';
 import { useRef } from 'react';
+import Modal from './Modal.jsx';
 
 
 export default function NewProject({onAdd}) {
+const modal = useRef()
+
 const title = useRef()
 const description = useRef()
 const dueDate = useRef()
@@ -12,14 +15,35 @@ function handleSave() {
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
 
+    if (
+        enteredTitle.trim() === '' ||
+        enteredDescription.trim() === '' ||
+        enteredDueDate.trim() === ''
+      ) {
+        modal.current.open();
+        return;
+      }
+
     onAdd({
         title: enteredTitle,
         description: enteredDescription,
         dueDate: enteredDueDate
+
+
     })
 }
 
   return (
+    <>
+     <Modal ref={modal} buttonCaption="Okay">
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
+          Oops ... looks like you forgot to enter a value.
+        </p>
+        <p className="text-stone-600 mb-4">
+          Please make sure you provide a valid value for every input field.
+        </p>
+      </Modal>
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
         <li>
@@ -37,5 +61,6 @@ function handleSave() {
         <Input type="date" ref={dueDate} label="Due Date" />
       </div>
     </div>
+    </>
   );
 }
