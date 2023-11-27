818c7e4fc050a874ffdb432f5ac23c408b18c45c
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-11-27 20:13:05 +0300

    Обработка удаления проекта
    Добавление «Задач проекта» и компонента «Задачи».
    Обработка удаления проекта

--- Раздел9_Practice/src/App.jsx
+++ Раздел9_Practice/src/App.jsx
@@ -11,21 +11,29 @@ function App() {
     projects: [],
   });
 
-
-function handleSelectProject(id){
-  setProjectsState((prevState) => {
-    return {
-      ...prevState,
-      selectedProjectId: id,
-    };
-  });
-}
+  function handleSelectProject(id) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  }
 
   function handleStartAddProject() {
     setProjectsState((prevState) => {
       return {
         ...prevState,
         selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleCancelAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
       };
     });
   }
@@ -46,14 +54,30 @@ function handleSelectProject(id){
     });
   }
 
+  function handleDeleteProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  }
+
   const selectedProject = projectsState.projects.find(
     (project) => project.id === projectsState.selectedProjectId
   );
 
-  let content = <SelectedProject project={selectedProject} />;
+  let content = (
+    <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
+  );
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} />;
+    content = (
+      <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
+    );
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
