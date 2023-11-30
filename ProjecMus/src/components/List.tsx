f771843f081b338e48d3b52d234efd6d0a38be90
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-11-30 13:15:34 +0300

    Add files via upload

---
+++ ProjecMus/src/components/List.tsx
@@ -0,0 +1,26 @@
+
+
+
+export const Lists = [
+    {
+      id: '1',
+      
+      title: 'Majestic Vintage Mocha Overcoat'
+      },
+    {
+      id: '2',
+      
+      title: 'Enchanting Blush Dream Gown'
+      },
+  
+    {
+      id: '3',
+      
+      title: 'Vintage Sunshine Rain Jacket'
+      },
+    {
+      id: '4',
+    
+      title: 'Classic Merlot Business Suit'
+        }
+    ]
