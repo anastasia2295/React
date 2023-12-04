2d1dcb8a230380cee50551156e5b1420ef3bf34e
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-12-04 18:24:13 +0300

    Add files via upload

--- ProjecMus/src/components/PlayerMaket.tsx
+++ ProjecMus/src/components/PlayerMaket.tsx
@@ -7,7 +7,7 @@ import { HiPlay } from "react-icons/hi";
 import { HiPause } from "react-icons/hi";
 
 
-export default function PlayerMaket () {
+export default function PlayerMaket ({ id, image, title, artist, time}) {
     
         const [isPlaying, setIsPlaying] = useState(false);
 
@@ -17,35 +17,34 @@ export default function PlayerMaket () {
            setIsPlaying(false);
          } else {
            setIsPlaying(true);
-           
          }
        };
   
   return (
         <div className="component">
-            <h2>id</h2>
-            <img className="musicCover" src={imageMusic} />
+            <h2>{id}</h2>
+            <img className="musicCover" src={image} />
                <div className="component_title">
-                   <h2 className="title">title</h2>
-                   <p className="subTitle">autor</p>
+                   <h2 className="title">{title}</h2>
+                   <p className="subTitle">{artist}</p>
                </div>
         <div>
             <div className="time">
               <p>
-                time
+                {time}
               </p>
             </div>
           </div>
           
           <div >
-          {!isPlaying ? (
-            <HiPlay size={40} onClick={playingButton}/>
+             {!isPlaying ? (
+                 <HiPlay size={40} onClick={playingButton}/>
         ) : (
             <div className="play">
               <div className="play1">
-          <HiStatusOnline size={40}/>
-               </div>
-            <HiPause size={40} onClick={playingButton}/>
+                  <HiStatusOnline size={40}/>
+              </div>
+                  <HiPause size={40} onClick={playingButton}/>
             </div>
         )}
         
