e433c7670c39ece1b9bdc486cdf80c919fc75a33
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-12-07 21:34:46 +0300

    корректировка верстки

--- ProjecMus/src/components/PlayerMaket.tsx
+++ ProjecMus/src/components/PlayerMaket.tsx
@@ -1,4 +1,3 @@
-import {  useState } from "react";
 import "./PlayList.css"
 import { Component } from "react";
 import "../components/PlayList.css"
@@ -6,10 +5,13 @@ import { CORE_CONCEPTS } from './data';
 import { HiStatusOnline } from "react-icons/hi";
 import { HiPlay } from "react-icons/hi";
 import { HiPause } from "react-icons/hi";
+import { CiPlay1 } from "react-icons/ci";
+import { CiPause1 } from "react-icons/ci";
+import { CiStreamOn } from "react-icons/ci";
+
 
 class PlayerMaket extends Component {
 
-    
     state = {
     activeItem: null,
     isPlaying: false
@@ -30,33 +32,39 @@ class PlayerMaket extends Component {
     
     key={index}
     >
-            <h2>{item.id}</h2>
+        <div className="component_id">
+            <h3>{item.id}</h3>
+            </div>
+             <div className="component_image">
             <img className="musicCover" src={item.image} />
+            </div>
                <div className="component_title">
-                   <h2 className="title">{item.title}</h2>
+                   <p className="title">{item.title} </p>
+                   <p> - </p>
                    <p className="subTitle">{item.artist}</p>
-               </div>
-        <div>
-            <div className="time">
+               
+        </div>
+        
+            <div className="component_time">
               <p>
                 {item. time}
               </p>
             </div>
-          </div>
-          
-          <div >
+          <div className="button">
           {activeItem === index && isPlaying===true ? (
+            
                    <div className="play">
                    <div className="play1">
-                       <HiStatusOnline size={40}/>
+                       <CiStreamOn size={35}/>
                    </div>
-                       <HiPause size={40} onClick={() => this.handlePausa(index)}/>
-                       
+                       <CiPause1 size={35} onClick={() => this.handlePausa(index)}/>
                  </div>
                   ) : (
-             <HiPlay size={40} onClick={() => this.handleClick(index)}/>
+             <CiPlay1  size={35} onClick={() => this.handleClick(index)}/>
+             
          )}
         </div>
+        
     </li>
     ))}
     </ul>
@@ -64,49 +72,3 @@ class PlayerMaket extends Component {
     }
     }
     export default PlayerMaket;
-    
-// export default function PlayerMaket ({ id, image, title, artist, time}) {
-    
-//         const [isPlaying, setIsPlaying] = useState(false);
-
-  
-//         const playingButton = () => {
-//          if (isPlaying) {
-//            setIsPlaying(false);
-//          } else {
-//            setIsPlaying(true);
-//          }
-//        };
-  
-//   return (
-//         <div className="component">
-//             <h2>{id}</h2>
-//             <img className="musicCover" src={image} />
-//                <div className="component_title">
-//                    <h2 className="title">{title}</h2>
-//                    <p className="subTitle">{artist}</p>
-//                </div>
-//         <div>
-//             <div className="time">
-//               <p>
-//                 {time}
-//               </p>
-//             </div>
-//           </div>
-          
-//           <div >
-//              {!isPlaying ? (
-//                  <HiPlay size={40} onClick={playingButton}/>
-//         ) : (
-//             <div className="play">
-//               <div className="play1">
-//                   <HiStatusOnline size={40}/>
-//               </div>
-//                   <HiPause size={40} onClick={playingButton}/>
-//             </div>
-//         )}
-        
-//         </div>
-//         </div>
-//       );
-// }
