a7fa4542bba4d01465863263a38a9e1575ed37e8
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-01-03 21:30:42 +0300

    Delete ProjecMus directory

--- ProjecMus/src/components/InputRange.tsx
+++
@@ -1,8 +0,0 @@
-import "./InputRange.css"
-
-export default function  InputRange()  {
-    return (
-    <div className="wrapper">
-  <input type="range" name="range1" min="1" max="100" step="1" value="5" />
-</div>
-)}
