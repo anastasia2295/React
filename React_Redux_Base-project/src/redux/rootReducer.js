3dbdd6a033805a6fa5c52b4a4b33135033bbc414
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-01-17 17:19:29 +0300

    Add files via upload

--- React_Redux_Base-project/src/redux/rootReducer.js
+++ React_Redux_Base-project/src/redux/rootReducer.js
@@ -1,5 +1,12 @@
 import {combineReducers} from "redux"
 import { likesReducer } from "./likesReducer"
+import { inputReducer } from "./inputReducer"
+import { commentsReducer } from "./commentsRedecer"
+import { appReducer } from "./appReducer"
+
 export const rootReducer = combineReducers({
- likesReducer
+ likesReducer,
+ inputReducer,
+ commentsReducer,
+ appReducer
 })
