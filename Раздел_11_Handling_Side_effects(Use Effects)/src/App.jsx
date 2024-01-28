60a5e4d94a7e112cfa38ba141f3b66680b92aa2f
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-01-28 21:41:01 +0300

    Add files via upload

--- Раздел_11_Handling_Side_effects(Use Effects)/src/App.jsx
+++ Раздел_11_Handling_Side_effects(Use Effects)/src/App.jsx
@@ -13,6 +13,12 @@ function App() {
   const [availablePlaces, setAvailablePlaces] = useState([])
   const [pickedPlaces, setPickedPlaces] = useState([]);
 
+  useEffect(()=>{
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    const storedPlaces = storedIds.map(id => 
+      AVAILABLE_PLACES.find((place) => place.id === id))
+      setPickedPlaces(storedPlaces)
+  }, [])
   useEffect(() =>{
     navigator.geolocation.getCurrentPosition((position) =>{
     const sortedPlaces = sortPlacesByDistance(
@@ -57,6 +63,9 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+  
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    localStorage.setItem("selectedPlaces", JSON.stringify(storedIds.filter((id) => id!== selectedPlace.current)))
   }
 
   return (
