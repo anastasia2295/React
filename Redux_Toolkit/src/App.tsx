bd7763da7b264421f4694edf3be5d6955c1ca08b
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-02-06 21:23:19 +0300

    Add files via upload

---
+++ Redux_Toolkit/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import { useAppDispatch, useAppSelector } from './hooks/redux';
+import { userSlice } from './reducers/UserSlice';
+
+
+function App() {
+  const{ count } = useAppSelector(state => state.userReducer)
+  const {increment} = userSlice.actions
+  const dispatch = useAppDispatch()
+  
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={()=> dispatch(increment(10))}>INCREMENT</button>
+    </div>
+  );
+}
+
+export default App;
