0641a9267cd1ea76e0317333131197d4f66f0663
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-03-04 19:22:56 +0300

    Add files via upload

---
+++ Redux/src/store/index.js
@@ -0,0 +1,23 @@
+import {createStore } from "redux"
+
+
+const counterReducer = (state = {counter: 0}, action) =>{
+    if(action.type === "INCREMENT"){
+        return {
+            counter: state.counter + 1
+    } 
+}
+     if(action.type === "DECREMENT") {
+        return {
+            counter: state.counter - 1
+        }
+     }
+     if(action.type === "INCREASE") {
+        return {
+            counter: state.counter + action.payload
+        }
+     }
+     return state
+}
+const store = createStore(counterReducer)
+export default store
