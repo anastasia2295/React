fd7672c40eae6d4e8fcc3827e5c292cc7a68603b
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-03-04 19:31:33 +0300

    Add files via upload

--- Redux/src/store/index.js
+++ Redux/src/store/index.js
@@ -1,20 +1,30 @@
 import {createStore } from "redux"
 
+const initialState = {counter: 0, showCounter: true}
 
-const counterReducer = (state = {counter: 0}, action) =>{
+const counterReducer = (state = initialState, action) =>{
     if(action.type === "INCREMENT"){
         return {
-            counter: state.counter + 1
+            counter: state.counter + 1,
+            showCounter: state.showCounter
     } 
 }
      if(action.type === "DECREMENT") {
         return {
-            counter: state.counter - 1
+            counter: state.counter - 1,
+            showCounter: state.showCounter
         }
      }
      if(action.type === "INCREASE") {
         return {
-            counter: state.counter + action.payload
+            counter: state.counter + action.payload,
+            showCounter: state.showCounter
+        }
+     }
+     if(action.type === "toggle") {
+        return {
+            showCounter: !state.showCounter,
+            counter: state.counter
         }
      }
      return state
