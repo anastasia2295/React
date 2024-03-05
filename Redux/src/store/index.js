0d6453103a6e9eb084032a3fd4ee8a6063db0157
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-03-05 21:01:19 +0300

    Add files via upload

--- Redux/src/store/index.js
+++ Redux/src/store/index.js
@@ -1,33 +1,13 @@
-import {createStore } from "redux"
+import { configureStore } from "@reduxjs/toolkit"
+import counterReducer from "./counter"
+import authReducer from "./auth"
 
-const initialState = {counter: 0, showCounter: true}
 
-const counterReducer = (state = initialState, action) =>{
-    if(action.type === "INCREMENT"){
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter
-    } 
-}
-     if(action.type === "DECREMENT") {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter
-        }
-     }
-     if(action.type === "INCREASE") {
-        return {
-            counter: state.counter + action.payload,
-            showCounter: state.showCounter
-        }
-     }
-     if(action.type === "toggle") {
-        return {
-            showCounter: !state.showCounter,
-            counter: state.counter
-        }
-     }
-     return state
-}
-const store = createStore(counterReducer)
+const store = configureStore({
+    reducer: {counter: counterReducer,
+    auth: authReducer}
+})
+
+
+
 export default store
