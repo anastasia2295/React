3ba76b672358ee51b4b39e9de6037e1febebc909
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-03-05 21:01:51 +0300

    Add files via upload

--- Redux/src/components/Counter.js
+++ Redux/src/components/Counter.js
@@ -1,12 +1,35 @@
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Counter.module.css';
+import { counterActions } from '../store/counter';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch()
+ const counter =  useSelector(state => state.counter.counter)
+ const show = useSelector(state=> state.counter.showCounter)
+
+ const incrementHandler = () =>{
+  dispatch(counterActions.increment())
+ }
+
+ const decrementHandler = () =>{
+  dispatch(counterActions.decrement())
+ }
+ const increaseHandler = () =>{
+  dispatch(counterActions.increase(10))
+ }
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter())
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>INCREMENT</button>
+        <button onClick={decrementHandler}>DECREMENT</button>
+        <button onClick={increaseHandler}>INCREASE</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- Redux/src/components/Header.js
+++ Redux/src/components/Header.js
@@ -1,9 +1,18 @@
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Header.module.css';
+import { authActions } from '../store/auth';
 
 const Header = () => {
+  const dispatch = useDispatch()
+  const isAuth = useSelector((state)=> state.auth.isAuthenticated)
+  const logoutAction = () =>{
+    dispatch(authActions.logout())
+  }
   return (
+    
     <header className={classes.header}>
       <h1>Redux Auth</h1>
+      {isAuth && (
       <nav>
         <ul>
           <li>
@@ -13,11 +22,13 @@ const Header = () => {
             <a href='/'>My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            <button onClick={logoutAction}>Logout</button>
           </li>
         </ul>
       </nav>
+      )}
     </header>
+  
   );
 };
 
