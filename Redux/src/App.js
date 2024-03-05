a3cab453b96e4cf44f035be572aab9de729c8a02
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-03-05 21:00:50 +0300

    Add files via upload

--- Redux/src/App.js
+++ Redux/src/App.js
@@ -1,10 +1,16 @@
-
+import Header from "./components/Header"
+import Auth from "./components/Auth"
+import { Fragment } from 'react';
 import Counter from './components/Counter';
 
 
 function App() {
   return (
+    <Fragment>
+      <Header/>
+      <Auth/>
     <Counter/>
+    </Fragment>
   );
 }
 
