baf779fe22350a0f1775a1edaecd43966d9c16fc
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-03-06 16:27:41 +0300

    Add files via upload

--- Redux_Advanced_20_part/src/store/card-slice.js
+++ Redux_Advanced_20_part/src/store/card-slice.js
@@ -39,13 +39,42 @@ const cartSlice = createSlice({
     }
  })
 
-const sendCartData = (cartData) => {
-    return (dispatch) =>{
+const sendCartData = (cart) => {
+    return async (dispatch) => {
         dispatch(uiAction.showNotification({
             status: "pending",
             title: "Sending...",
             message: "Sending cart data"
-          }))
+          })
+        )
+        const sendRequest = async () =>{
+          const response = await fetch(
+            "https://react-http-6b4a6.firebaseio.com/cart.json", 
+            {
+          method: "PUT",
+          body: JSON.stringify(cart)
+        })
+        if(!response.ok) {
+          throw new Error("Sending cart data failed")
+        }  
+        }
+        try{
+            await sendRequest()
+            dispatch(uiAction.showNotification({
+            status: "success",
+            title: "Success",
+            message: "Sent cart data successfully"
+          })
+          )
+        } catch (error){
+            dispatch(uiAction.showNotification({
+                status: "Error",
+                title: "Error...",
+                message: "Sending cart data failed"  
+              })
+              )
+        }
+        
     }
 }
 
