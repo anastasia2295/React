67f7e9b4780e1ac17b4e341d36281e43b9c5063e
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-03-10 17:47:55 +0300

    Add files via upload

---
+++ src/components/AvailablePlaces.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import Places from './Places.jsx';
+import { useState } from 'react';
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([])
+
+  useEffect(() =>{
+    fetch("http://localhost:3000/places")
+      .then((response) =>{
+        return response.json();
+      })
+      .then((resData) =>{
+        setAvailablePlaces(resData.places)
+      })
+  }, [])
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
