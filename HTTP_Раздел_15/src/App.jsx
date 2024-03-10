c92ae97df81c9fe6dbc03f1f7648750702d6daf2
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-03-10 17:51:09 +0300

    Add files via upload

--- HTTP_Раздел_15/src/App.jsx
+++ HTTP_Раздел_15/src/App.jsx
@@ -5,12 +5,14 @@ import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
+import { updateUserPlaces } from './http.js';
+import Error from './components/Error.jsx';
 
 function App() {
   const selectedPlace = useRef();
 
   const [userPlaces, setUserPlaces] = useState([]);
-
+  const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState()
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   function handleStartRemovePlace(place) {
@@ -22,7 +24,7 @@ function App() {
     setModalIsOpen(false);
   }
 
-  function handleSelectPlace(selectedPlace) {
+  async function handleSelectPlace(selectedPlace) {
     setUserPlaces((prevPickedPlaces) => {
       if (!prevPickedPlaces) {
         prevPickedPlaces = [];
@@ -32,18 +34,51 @@ function App() {
       }
       return [selectedPlace, ...prevPickedPlaces];
     });
+
+    try{
+      await updateUserPlaces([selectedPlace, ...userPlaces])
+    } catch (error) {
+      setUserPlaces(userPlaces)
+      setErrorUpdatingPlaces({
+        message: error.message || "Failed to update places"
+      })
+    }
+    
   }
 
   const handleRemovePlace = useCallback(async function handleRemovePlace() {
     setUserPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
-    );
+    ); 
+
+    try{
+      await updateUserPlaces(
+      userPlaces.filter((place) => place.id !==selectedPlace.current.id)
+    )
+    }catch(error) {
+      setUserPlaces(userPlaces)
+      setErrorUpdatingPlaces({
+        message: error.message || "Failed to delete place"
+      })
+    }
+    
 
     setModalIsOpen(false);
-  }, []);
+  }, [userPlaces]);
+  
+  function handleError() {
+    setErrorUpdatingPlaces(null)
+  }
 
   return (
     <>
+    <Modal open={errorUpdatingPlaces} onClose={handleError}>
+      {errorUpdatingPlaces && <Error
+      title="An error occurred"
+      message= {errorUpdatingPlaces.message}
+      onConfirm={handleError}
+      />}
+    </Modal>
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
