4a868fc23e9eef4e5d993a903627f8224867842a
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-03-11 19:50:24 +0300

    Add files via upload

---
+++ SPA_Раздел_21_parte2/frontend/src/pages/EditEvent.js
@@ -0,0 +1,9 @@
+
+function EditEvent() {
+    return( 
+       <>
+           <h1>EditEvent</h1>
+       </>
+    )}
+    
+    export default EditEvent
---
+++ SPA_Раздел_21_parte2/frontend/src/pages/NewEvent.js
@@ -0,0 +1,9 @@
+
+function NewEvent() {
+return( 
+   <>
+       <h1>NewEvent</h1>
+   </>
+)}
+
+export default NewEvent
