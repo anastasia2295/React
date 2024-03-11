36c633bc69521d514210fcad540ba0544a40ef99
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-03-11 19:50:56 +0300

    Add files via upload

--- SPA_Раздел_21_parte2/frontend/src/components/MainNavigation.js
+++ SPA_Раздел_21_parte2/frontend/src/components/MainNavigation.js
@@ -1,19 +1,33 @@
 import classes from './MainNavigation.module.css';
+import { NavLink } from "react-router-dom";
+
 
 function MainNavigation() {
   return (
+    <>
     <header className={classes.header}>
       <nav>
-        <ul className={classes.list}>
-          <li>
-            <a>Home</a>
-          </li>
-          <li>
-            <a>Events</a>
-          </li>
-        </ul>
+      <ul className={classes.list}>
+                <li><NavLink to="/"
+                className={({isActive})=>
+                   isActive? classes.active : undefined
+                }
+                end
+                >Home
+                </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/events"
+                    className={({isActive})=>
+                    isActive? classes.active : undefined
+                 }
+                    >Products
+                    </NavLink>
+                </li>
+            </ul>
       </nav>
     </header>
+    </>
   );
 }
 
