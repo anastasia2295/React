4982c5a2de6613f6421d3fc6346f7028a7dd45d7
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-03-13 21:33:49 +0300

    Add files via upload

---
+++ SPA_Раздел_21_parte2/frontend/src/pages/Event.js
@@ -0,0 +1,32 @@
+
+import { useLoaderData, json } from 'react-router-dom';
+import EventsList from '../components/EventsList';
+
+function EventsPage() {
+    const events = useLoaderData()
+  return (
+      <EventsList events={events} />
+  );
+}
+
+export default EventsPage;
+
+export async function loader() {
+    const response = await fetch('http://localhost:8080/events');
+
+      if (!response.ok) {
+        // return {isError: true, message: "Could not fetch events"};
+        // throw new Response (JSON.stringify({message: "Could not fetch events"}),{
+        // status: 500,
+        // })
+        return json(
+            {message: "Could not fetch events"},
+            {
+                status: 500,
+            }
+        )
+      } else {
+        const resData = await response.json();
+        return resData.events
+      }
+}
