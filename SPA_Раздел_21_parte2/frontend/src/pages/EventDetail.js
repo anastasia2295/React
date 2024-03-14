9ae470c2d6f0b93917103d07b19b45586184342a
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-03-14 19:44:48 +0300

    Add files via upload

--- SPA_Раздел_21_parte2/frontend/src/pages/EventDetail.js
+++ SPA_Раздел_21_parte2/frontend/src/pages/EventDetail.js
@@ -1,4 +1,4 @@
-import { json, useRouteLoaderData } from "react-router-dom"
+import { json, redirect, useRouteLoaderData } from "react-router-dom"
 import EventItem from "../components/EventItem"
 
 function EventDetail() {
@@ -23,4 +23,19 @@ function EventDetail() {
         }else {
                 return response
             }
+    }
+
+    export async function action ({params}) {
+        const eventId = params.eventId
+        const response = await fetch("http:/localhost:8080/events/" + eventId, {
+            method: "DELETE"
+        })
+        if(!response.ok) {
+            throw json({message: "Could not delete event"},
+            {
+                status: 500
+            })
+        }
+
+    return redirect("/events")
     }
--- SPA_Раздел_21_parte2/frontend/src/pages/NewEvent.js
+++ SPA_Раздел_21_parte2/frontend/src/pages/NewEvent.js
@@ -1,4 +1,5 @@
 import EventForm from "../components/EventForm";
+import { json, redirect } from "react-router-dom";
 
 function NewEvent() {
 return( 
@@ -8,3 +9,24 @@ return(
 )}
 
 export default NewEvent
+
+export async function action({request, params}) {
+    const data = await request.formData()
+    const eventData = {
+        title: data.get("title"),
+        image: data.get("image"),
+        date: data.get("date"),
+        description: data.get("description"),
+    }
+    const response = fetch("http:/localhost:8080/events", {
+        method: "POST",
+        headers: {
+            "Content-Type": "aplication/json"
+        },
+        body: JSON.stringify(eventData)
+    })
+    if(!response.ok) {
+        throw json({message: "Coukd not save event"}, {status: 500})
+    }
+    return redirect("/events")
+}
