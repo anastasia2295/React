cd80c30e9b0bb51bb90548b0482192aac8823716
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-03-14 19:44:06 +0300

    Add files via upload

--- SPA_Раздел_21_parte2/frontend/src/App.js
+++ SPA_Раздел_21_parte2/frontend/src/App.js
@@ -1,11 +1,14 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/Home";
 import RootLayout from "./pages/Root";
-import ErrorPage from "./pages/Error";
-import Events from "./pages/Events";
-import EventDetail from "./pages/EventDetail";
-import NewEvent from "./pages/NewEvent";
+import EventDetail, { 
+  loader as eventDetailLoader,
+  action as deleteEventAction
+ } from "./pages/EventDetail";
+import NewEvent, {action as NewEventAction} from "./pages/NewEvent";
 import EditEvent from "./pages/EditEvent";
+import EventsRootLayout from "./pages/EventsRoot";
+import EventsPage, {loader as eventsLoader} from "./pages/Event";
 
 
 
@@ -15,10 +18,28 @@ const router = createBrowserRouter([
     element: <RootLayout/>,
     children: [
      {index: true, element: <HomePage/>},
-     {path: "events", element: <Events/>},
-     {path: "events/:event.id", element:<EventDetail/>},
-     {path: "events/new", element:<NewEvent/>},
-     {path: "events/:eventId/edit", element:<EditEvent/>}
+     {path: "events", 
+     element: <EventsRootLayout/>,
+     children: [
+     {index: true, 
+      element: <EventsPage/>, 
+      loader: eventsLoader
+    },
+     {
+      path: ":eventId",
+      id: "event-detail",
+      loader: eventDetailLoader,
+      children:[
+         {index: true, 
+          element:<EventDetail/>,
+          action: deleteEventAction
+          },
+         {path: "edit", element:<EditEvent/>}, 
+      ]
+     },
+     {path: "new", element:<NewEvent/>, action: NewEventAction},
+     ]
+    }
     ]
   }
 ])  
