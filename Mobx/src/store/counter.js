4d9bae363a3d814cf580c7c5ea429b36d4fd596f
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-03-25 18:40:14 +0300

    Add files via upload

--- Mobx/src/store/counter.js
+++ Mobx/src/store/counter.js
@@ -1,7 +1,8 @@
-import {makeAutoObservable, makeObservable} from "mobx"
+import {makeAutoObservable, } from "mobx"
 
 class Counter {
     count = 0
+    timer = 60
     constructor() {
         makeAutoObservable(this)
     }
@@ -10,6 +11,17 @@ class Counter {
     }
     decrement() {
         this.count = this.count - 1 
+    }
+    get total() {
+        return `Count +timer =` + this.timer + this.count
+    }
+
+    fetchTodo() {
+        fetch("https:/jsonplaceholder.typicode.com/todos")
+            .then(response => response.json())
+            .then(json => {
+                this.todos = [...this.todos, ...json]
+            })
     }
 }
 export default new Counter()
