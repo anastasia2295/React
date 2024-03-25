1c5a5520a960f297e2af52e456c2d242da612ef5
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-03-25 19:32:47 +0300

    Add files via upload

---
+++ agile-board/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div>
+     start
+    </div>
+  );
+}
+
+export default App;
---
+++ agile-board/src/store/index.js
@@ -0,0 +1,8 @@
+import { types } from "mobx-state-tree";
+
+const RootStore = types.model("RootStore", {
+    users: UsersStore,
+    boards: BoardStore,
+})
+
+export default RootStore
